7302ccab4f8b38153778c53ec5c534ea13a97746
James <entreur@gmail.com>
2023-04-15 22:39:30 +0400

    Variables Lesson

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿ class Program
+{
+    public static void Main()
+    {
+        string userInput; //User input can never be null. Don't try to null check user input even if not initialized.
+        Console.WriteLine("What is your age?");
+        userInput = Console.ReadLine();
+        if (IsValidNumber(userInput ?? ""))
+        {
+            string result = string.Format("Your age is {0}", userInput);
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine("Please use numbers (1 to 9)");
+        }       
+    }
+
+    static bool IsValidNumber (string value)
+    {
+        if(value != "")
+        {
+            return int.TryParse(value, out int result);
+        }
+        return false;       
+    }
+}
+
