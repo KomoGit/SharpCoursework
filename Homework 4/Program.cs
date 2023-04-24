4597d5e48ffb55790e835e620111cdfdc614dc8f
James <entreur@gmail.com>
2023-04-24 18:05:02 +0400

    Newest homework

---
+++ Homework 4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{  
+    internal class Program
+    {
+        private static FirstHomework firstHomework = new FirstHomework();
+        private static SecondHomework secondHomework = new SecondHomework();
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 7, 9, 12, 14 };
+            //firstHomework.DividesToThree(7,18);
+            secondHomework.MinimumMaximum(arr);
+        }
+    }
+}
+
+class FirstHomework
+{
+    public void DividesToThree(int range1, int range2)
+    {
+        for (int i = range1; i <= range2; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }           
+        }
+    }
+}
+
+class SecondHomework
+{
+    public void MinimumMaximum(int[] arr)
+    {
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];             
+            }
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        Console.WriteLine(min);
+        Console.WriteLine(max);
+    }
+}
+
+class ThirdHomework
+{
+
+}
+
+class FourthHomework
+{
+
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9,10};
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    Console.WriteLine(nums[i]);
+                }
+            }
+        }
+    }
+}
