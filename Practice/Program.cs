77b42cb4176184bf97a7f1f75dbc843f1c8e3d79
James <entreur@gmail.com>
2023-05-03 14:56:29 +0400

    Newest practice

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,8 +6,11 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9,10};
-
+            ThirdPractice third = new ThirdPractice();
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9,10};
+            char[] chars = { 'a', 'b', 'c', 'd', 'e' };
+            string[] strings = {"a","b","c","d","e" };
+            Console.WriteLine(third.FindIndexOfElement(strings,"a"));
             //for (int i = 0; i < nums.Length; i++)
             //{
             //    if (nums[i] % 2 == 0)
@@ -32,12 +35,27 @@ namespace Practice
         public float Area(float a,float b,float c,float r,float p)
         {
             p = (a + b + c) / 2;
-            var s = p * r; //Cannot return s? What purpose does it have here?      
+            var s = p * r;   
             return s;
         }
         public float DArea(float a, float b)
         {
             return a * b;
         }
+    }
+}
+
+internal class ThirdPractice 
+{ 
+    public int FindIndexOfElement<T>(T[] charArr, dynamic identifier)
+    {
+        for (int i = 0; i < charArr.Length; i++)
+        {
+            if (charArr[i] == identifier)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
