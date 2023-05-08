f27f6e9fb5bb2efae6d80a2d000a425e16e64552
James <entreur@gmail.com>
2023-05-08 13:48:13 +0400

    Newest Homework

---
+++ Chapter 2/Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Use Test cases.");
+        }
+    }
+}
+
+internal class FirstHomework
+{
+    public float SumASCIIValues(string text)
+    {
+        if(text == null)
+        {
+            throw new Exception("Value cannot be null");
+        }
+        float result = default;
+        byte[] asciiBytes = Encoding.ASCII.GetBytes(text);
+        for (int i = 0; i < asciiBytes.Length; i++)
+        {
+            result += asciiBytes[i];
+        }
+        return result;
+    }
+}
---
+++ Chapter 2/Homework/Test.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Homework
+{
+    [TestFixture]
+    internal class Test
+    {
+        private readonly FirstHomework first = new FirstHomework();
+        [TestCase]
+        public void TestHelloWorld()
+        {
+            string inserted = "Hello World";
+            float expected = 1052;
+            Assert.AreEqual(expected, first.SumASCIIValues(inserted));
+        }
+
+        [TestCase]
+        public void Testhelloworld()
+        {
+            string inserted = "hello world";
+            float expected = 1116f;
+            Assert.AreEqual(expected, first.SumASCIIValues(inserted));
+        }
+
+        [TestCase]
+        public void TestNull()
+        {
+            string inserted = default;
+            Assert.Throws<Exception>(() => first.SumASCIIValues(inserted));
+        }
+
+        [TestCase]
+        public void TestNothing()
+        {
+            string inserted = "";
+            float expected = 0f;
+            Assert.AreEqual(expected, first.SumASCIIValues(inserted));
+        }
+    }
+}
