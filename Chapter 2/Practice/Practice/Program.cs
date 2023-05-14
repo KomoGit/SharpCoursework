33dce92b9633f50de39592b086df9c825e4d90db
James <entreur@gmail.com>
2023-05-14 21:59:38 +0400

    Second practice

--- Chapter 2/Practice/Practice/Program.cs
+++ Chapter 2/Practice/Practice/Program.cs
@@ -6,11 +6,16 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            FirstPractice first = new FirstPractice();
-            char[] index = { 'c','g' };
+            //FirstPractice first = new FirstPractice();
+            //char[] index = { 'c','g' };
             //Console.WriteLine(first.GetLastName("James Clyde Henry"));
             //Console.WriteLine(first.GetLastName("Walter White Hartwell"));
-            Console.WriteLine(first.FindRepatedStrings("afcgafcgafcgafafafcgafafafcg",index));
+            //Console.WriteLine(first.FindRepatedStrings("afcgafcgafcgafafafcgafafafcg",index));
+
+            SecondPractice second = new SecondPractice();
+            //second.ReturnRandomCollection(10);
+            //second.ReturnRandomCharacther(10);
+            second.ReturnRandomCombination(10);
         }
     }
 
@@ -35,6 +40,68 @@ namespace Practice
                 }
             }
             return res;
+        }
+    }
+
+    internal class SecondPractice
+    {
+        private readonly Random rnd = new Random();
+
+        public void ReturnRandomCollection(int randomRange)
+        {
+            int prev = 0;
+            for (int i = 0; i < randomRange; i++)
+            {
+                int current = rnd.Next(1, randomRange);
+                if (current != prev)
+                {
+                    Console.Write(current);
+                    prev = current;
+                }              
+            }
+            Console.WriteLine("");
+        }
+        
+        public void ReturnRandomCharacther(int randomRange)
+        {
+            char prev = default;
+            for (int i = 0; i < randomRange; i++)
+            {
+                char current = (char)rnd.Next(1, randomRange);
+                if (current != prev)
+                {
+                    Console.Write(current);
+                    prev = current;
+                }
+            }
+            Console.WriteLine("");
+        }
+
+        public void ReturnRandomCombination(int randomRange)
+        {
+            bool swtc = default;
+            for (int i = 0; i < randomRange; i++)
+            {
+                if (swtc)
+                {
+                    Console.WriteLine((char)rnd.Next(1,randomRange));
+                    swtc = false;
+                }
+                else
+                {
+                    Console.WriteLine(rnd.Next(1,randomRange));
+                    swtc = true;
+                }
+            }
+            //for (int i = 0; i < randomRange; i++)
+            //{
+            //    char current = (char)rnd.Next(1, randomRange);
+            //    if (current != prev)
+            //    {
+            //        Console.Write(current);
+            //        prev = current;
+            //    }
+            //}
         }
     }
 }
