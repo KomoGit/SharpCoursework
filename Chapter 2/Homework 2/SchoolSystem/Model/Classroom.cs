eb92c1139e55d2adf149c75e1e2e86ffc4bf14c4
James <entreur@gmail.com>
2023-05-14 14:11:37 +0400

    Homework done

---
+++ Chapter 2/Homework 2/SchoolSystem/Model/Classroom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolSystem.Model
+{
+    public class Classroom
+    {
+        public int Id { get; set; }
+        public Teacher Teacher;
+        private readonly List<Student> Students = new List<Student>();
+
+        public Classroom(int id, Teacher teacher,List<Student>students)
+        {
+            Id = id;
+            Teacher = teacher;
+            Students = students;          
+        }
+
+        public string GetTeacherFullName()
+        {
+            return Teacher.GetFullName();
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            
+            if(Students == null) return Students;
+            throw new Exception("No students available in this class");
+        }
+    }
+}
---
+++ Chapter 2/Homework 2/SchoolSystem/Tests/Test.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using SchoolSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Tests
+{
+    [TestFixture]
+    public class TestStudent
+    {
+        private Student student = new Student("James","Clyde",14,true);
+        private Student student2 = new Student("Jane", "Clyde", 15, false);
+        [TestCase]
+        public void TestGetStudentFullName()
+        {
+            string expected = "James Clyde";
+            Assert.AreEqual(expected,student.GetFullName());
+        }
+        [TestCase]
+        public void TestGetStudentGender()
+        {
+            string expected = "Female";
+            Assert.AreEqual(expected,student2.GetStudentGender());
+        }
+    }
+    public class TestTeacher
+    {
+        Teacher teacher = new Teacher("Megan","Idunno","History");
+        [TestCase]
+        public void TestGetTeacherFullName()
+        {
+            string expected = "Megan Idunno";
+            Assert.AreEqual(expected, teacher.GetFullName());
+        }
+    }
+    public class TestClassroom
+    {
+        private readonly Student student = new Student("James", "Clyde", 14, true);
+        private readonly Student student2 = new Student("Jane", "Clyde", 15, false);
+        static readonly Teacher teacher = new Teacher("Megan", "Idunno", "History");
+        static readonly List<Student> students = new List<Student>();
+        Classroom room = new Classroom(15, teacher: teacher, students);
+
+        [TestCase]
+        public void TestGetAllStudents()
+        {
+            students.Add(student);
+            students.Add(student2);
+            Assert.AreEqual(students,room.GetAllStudents());
+        }
+        [TestCase] 
+        public void TestGetAllStudentsEmpty()
+        {
+            Classroom room = new Classroom(15, teacher, students);
+            Assert.Throws<Exception>(() => room.GetAllStudents());
+        }
+        [TestCase] 
+        public void TestGetTeacherFullName()
+        {
+            string expected = "Megan Idunno";
+            Assert.AreEqual(expected,room.GetTeacherFullName());
+        }
+
+    }
+}
