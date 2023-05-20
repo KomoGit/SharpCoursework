4134b1435e5079edb8da50e2a93220d97610112a
James <entreur@gmail.com>
2023-05-20 21:56:54 +0400

    New practice done.

---
+++ Chapter 2/Practice/Practice 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student("Walter","White","Chemistry",100,true);
+            Console.WriteLine(stu.GetStudentInfo());
+            Console.WriteLine(stu.HasStudentGraduated());
+            Console.WriteLine(stu.GetDiplomaDegree());
+        }
+    }
+}
---
+++ Chapter 2/Practice/Practice 2/Student.cs
@@ -0,0 +1,36 @@
+﻿namespace Practice_2
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Group { get;private set; }
+        public float GPA { get; private set; }
+        private readonly bool HasGraduated = false;
+
+        public Student(string name,string surname,string group,float gpa,bool hasGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            GPA = gpa;
+            HasGraduated = hasGraduated;
+        }
+
+        public string GetStudentInfo()
+        {
+            return $"{Name} {Surname} {Group} {GPA} {HasGraduated}";
+        }
+
+        public string GetDiplomaDegree()
+        {
+            if (GPA > 90) return "With honors";
+            return "Lol loser, couldn't even graduate with honors";
+        }
+
+        public bool HasStudentGraduated()
+        {
+            return HasGraduated;    
+        }
+    }
+}
