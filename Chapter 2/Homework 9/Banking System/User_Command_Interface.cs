7403d994954124c4b1fe392055caccb5af0a2be2
E.A Nasir <example@example.com>
2023-06-22 12:47:58 +0400

    .

---
+++ Chapter 2/Homework 9/Banking System/User_Command_Interface.cs
@@ -0,0 +1,38 @@
+﻿namespace Banking_System
+{
+    public class User_Command_Interface
+    {
+        public static void StartApplication(ref bool isRunning)
+        {
+            while (isRunning)
+            {
+                Console.WriteLine("Hello, press enter: ");
+                string userInput = Console.ReadLine()!;
+                InterpretUserInput(userInput);
+            }
+        }
+
+        private static void InterpretUserInput(string Input)
+        {
+            char input = Convert.ToChar(Input.ToLower());
+            switch (input)
+            {
+                case 'A':
+                    Console.WriteLine(input);
+                    break;
+                case 'B':
+                    Console.WriteLine(input);
+                    break;
+                case 'C':
+                    Console.WriteLine(input);
+                    break;
+                case 'D':
+                    Console.WriteLine(input);
+                    break;
+                case 'E':
+                    Console.WriteLine(input);
+                    break;
+            }
+        }
+    }
+}
