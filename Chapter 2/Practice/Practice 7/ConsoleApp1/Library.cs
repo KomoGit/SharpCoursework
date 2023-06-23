a0a5029fc0c74c1d580cf639bec9d03295c92834
Elnur <entreur@gmail.com>
2023-06-23 13:32:54 +0400

    .

---
+++ Chapter 2/Practice/Practice 7/ConsoleApp1/Library.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    class Library
+    {
+        private List<Book> AllBooks { get; set; }
+
+        public Library()
+        {
+            AllBooks = new List<Book>();
+        }
+
+        public void AddBooks(Book book)
+        {
+            AllBooks.Add(book);
+        }
+
+        public void ShowAllBooks()
+        {
+            foreach (Book book in AllBooks)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
