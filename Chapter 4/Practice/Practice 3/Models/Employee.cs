7ddc86fc550a5460d5ecd7ffe0c99956bacd438b
Elnur <entreur@gmail.com>
2023-08-03 21:00:26 +0400

    .

---
+++ Chapter 4/Practice/Practice 3/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practice_3.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name cannot be empty!")]
+        [MaxLength(100, ErrorMessage = "Cannot be above 100")]
+        public string Fullname { get; set; }
+        [Required(ErrorMessage = "Cannot be empty!")]
+        public int PositionId { get; set; }
+        public Position Position { get; set; }
+    }
+}
---
+++ Chapter 4/Practice/Practice 3/Models/Position.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practice_3.Models
+{
+    public class Position
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name cannot be empty!")]
+        [MaxLength(100, ErrorMessage = "Cannot be above 100")]
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
