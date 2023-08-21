ff0ee9cce1743a8bea0f3f972bd2d886c834ae38
Elnur <entreur@gmail.com>
2023-08-21 11:33:08 +0400

    Now a partial class

--- Chapter 4/Homework 4/AngerTravelTours/AngerTravelTours/Data/AngerDbContext.cs
+++ Chapter 4/Homework 4/AngerTravelTours/AngerTravelTours/Data/AngerDbContext.cs
@@ -3,25 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AngerTravelTours.Data
 {
-    public class AngerDbContext:DbContext
+    public partial class AngerDbContext:DbContext
     {
         public AngerDbContext(DbContextOptions<AngerDbContext> options):base(options)
         {
 
         }
-        public DbSet<User> Users { get; set; }
-        public DbSet<About> About { get; set; }
-        public DbSet<Blog> Blogs { get; set; }
-        public DbSet<Category> Category { get; set; }
-        public DbSet<ChooseUs> ChooseUs { get; set; }
-        public DbSet<Gallery> Galleries { get; set; }
-        public DbSet<Partner> Partners { get; set; }
-        public Subscriber Subscriber { get; set; } //was a DbSet<> before
-        public DbSet<TourPackage> TourPackages { get; set; }
-        public DbSet<Contact> Contacts { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<Social> Socials { get; set; }
-        public DbSet<Feedback> Feedbacks { get; set; }
-        public DbSet<Service> Services { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<About> About { get; set; }
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<ChooseUs> ChooseUs { get; set; }
+        public virtual DbSet<Gallery> Galleries { get; set; }
+        public virtual DbSet<Partner> Partners { get; set; }
+        public virtual DbSet<Subscriber> Subscribes { get; set; }
+        public virtual DbSet<TourPackage> TourPackages { get; set; }
+        public virtual DbSet<Contact> Contacts { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Social> Socials { get; set; }
+        public virtual DbSet<Feedback> Feedbacks { get; set; }
+        public virtual DbSet<Service> Services { get; set; }
     }
 }
