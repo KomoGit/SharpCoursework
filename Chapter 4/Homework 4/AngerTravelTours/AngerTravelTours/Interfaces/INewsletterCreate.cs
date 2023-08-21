e5773f765d9c28cb1e409bb4a7875e727daa30f6
Elnur <entreur@gmail.com>
2023-08-21 11:34:25 +0400

    .

---
+++ Chapter 4/Homework 4/AngerTravelTours/AngerTravelTours/Interfaces/INewsletterCreate.cs
@@ -0,0 +1,10 @@
+﻿using AngerTravelTours.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngerTravelTours.Interfaces
+{
+    public interface INewsletterCreate
+    {
+        public IActionResult Create(VMBase sub);
+    }
+}
---
+++ Chapter 4/Homework 5/Register And Login/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Register_And_Login.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
