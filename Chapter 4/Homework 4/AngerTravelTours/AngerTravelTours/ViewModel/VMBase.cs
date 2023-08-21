c2ad0685e6a2460487874d39d860327e5072f088
Elnur <entreur@gmail.com>
2023-08-21 11:27:54 +0400

    is no longer abstract class

--- Chapter 4/Homework 4/AngerTravelTours/AngerTravelTours/ViewModel/VMBase.cs
+++ Chapter 4/Homework 4/AngerTravelTours/AngerTravelTours/ViewModel/VMBase.cs
@@ -2,12 +2,12 @@
 
 namespace AngerTravelTours.ViewModel
 {
-    public abstract class VMBase
+    public class VMBase
     {
         public List<Blog>? RecentPosts { get; set; }
         public List<Partner>? Partners { get; set; }
         public List<Feedback>? Feedbacks { get; set; }
-        public Subscriber? Subscriber { get; set; } //turn it into list maybe?
+        public Subscriber? Subscriber { get; set; }
         public List<Social>? Socials { get; set; }
     }
 }
